24326450c54f646af17ae2e6453273927bbd7983
drucix <284508@student.pwr.edu.pl>
2025-11-03 23:01:18 +0100

    osobna klasa do tworzenia okienka

---
+++ src/org/backround/FrameSettings.java
@@ -0,0 +1,10 @@
+package org.backround;
+import javax.swing.JFrame;
+
+public class FrameSettings extends JFrame {
+    FrameSettings() {
+        this.setVisible(true);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setSize(400, 400);
+    }
+}
--- src/org/backround/Main.java
+++ src/org/backround/Main.java
@@ -3,9 +3,6 @@ import javax.swing.JFrame;
 
 public class Main {
     public static void main(String[] args) {
-        JFrame frame = new JFrame();
-        frame.setVisible(true);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(400, 400);
+        FrameSettings frameSettings = new FrameSettings();
     }
 }
